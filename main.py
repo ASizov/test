a0a3b9767b64586a6354d5e9f56574f501d11786
asizov <aclion@yandex.ru>
2019-07-17 01:14:29 +0300

    unit tests

---
+++ main.py
@@ -0,0 +1,25 @@
+def max_substring_with_k_unique(s):
+  if not isinstance(s, str):
+      return 'string expected'
+  curr_sub, result_sub = [], []
+  max_len = -1
+  for i in range(0, len(s)):
+    curr_sub.append(s[i])
+    if len(set(curr_sub)) <= 2:
+      if len(curr_sub) >= max_len:
+        max_len = len(curr_sub)
+        result_sub = curr_sub[:]
+    else:
+      while len(set(curr_sub)) != 2:
+        curr_sub = curr_sub[1:]
+      if max_len <= len(curr_sub) and len(set(curr_sub)) <= 2:
+        max_len =  len(curr_sub)
+        result_sub = curr_sub[:]
+  result = ''
+  for x in result_sub:
+    result = result + x
+  return result
+
+if __name__ == '__main__':
+    s='aadabbaba'
+    print(max_substring_with_k_unique(s))
---
+++ unit_tests.py
@@ -0,0 +1,28 @@
+import unittest
+from main import max_substring_with_k_unique
+
+class TestCases(unittest.TestCase):
+
+    def test_case_begin(self):
+        self.assertEqual(max_substring_with_k_unique('aadaddabbaba'), 'aadadda')
+    
+    def test_case_end(self):
+        self.assertEqual(max_substring_with_k_unique('aadabbaba'), 'abbaba')
+        
+    def test_case_mid(self):
+        self.assertEqual(max_substring_with_k_unique('aadabddadaaadbbaba'), 'ddadaaad')
+        
+    def test_short_str(self):
+        self.assertEqual(max_substring_with_k_unique('a'), 'a')
+        
+    def test_empty_str(self):
+        self.assertEqual(max_substring_with_k_unique(''), '')
+        
+    def test_unique_char_less_k(self):
+        self.assertEqual(max_substring_with_k_unique('aaaaaaaaaaa'), 'aaaaaaaaaaa')
+        
+    def test_input_not_str(self):
+        self.assertEqual(max_substring_with_k_unique(777), 'string expected')
+
+if __name__ == '__main__':
+    unittest.main()
